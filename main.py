f0052e4763f604ad246cbc02dfb6687ff1ad7484
fausto-gnato-eng <faustojcl@gmail.com>
2023-02-12 22:25:15 -0300

    atual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-img = cv.imread("/home/takion/Documentos/Pixel/Fausto.jpeg")
+img = cv.imread("Endereço da imagem/imagem.extenção")
 cv.imshow("Imagem Original", img)
 
 cv.waitKey(0)
