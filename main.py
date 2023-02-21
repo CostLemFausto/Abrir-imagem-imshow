2a0c08510a81032cd83fc5e8ac12c46eb5d92eef
fausto-gnato-eng <faustojcl@gmail.com>
2023-02-20 21:59:42 -0300

    a

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# Este software garrega uma imagem na tela, mostra no terminal
-# sua  dimensão em pixels, e também pode redimensionar a mesma
-#*************************************************************
 import cv2 as cv
 
 img = cv.imread("Endereço da imagem/imagem.extenção")
-def dimensao():
-    pass
-
 cv.imshow("Imagem Original", img)
 
 cv.waitKey(0)
